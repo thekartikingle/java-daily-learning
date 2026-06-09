d9fa03c40e9aa99ef2639937cd69324c2ef9c33a
Kartik Ingle <thekartikingle@gmail.com>
2026-06-09 23:43:49 +0530

    Add files via upload
    start

---
+++ onefprogram.java
@@ -0,0 +1,41 @@
+
+public class onefprogram {
+ public static void main(String[] args) {
+	
+//	 1
+//	 01
+//	 101
+//	 0101
+//	 10101
+	 
+//	 for(int i=1;i<=5;i++)
+//	 {
+//		 for(int j=1;j<=i;j++)
+//			 if((i+j)%2==0)
+//				 System.out.print("1");
+//			 else
+//				 System.out.print("0");
+//		 
+//		 System.out.println();
+//	 }
+	 
+	 
+//	 13579
+//	 1357
+//	 135
+//	 13
+//	 1
+	 
+	
+	 for(int i=5;i>=1;i--)
+	 { int n=1;
+		 for(int j=1;j<=i;j++){
+			 System.out.print(n);
+		 n=n+2;}
+		 System.out.println();
+	 }
+	 
+	 
+	 
+}
+}
